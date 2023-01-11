eff5d1dd561265c90ef3493f8c7a785fe0d1fcd5
omer-ayhan <om.ayhan247@gmail.com>
2023-01-12 01:49:25 +0300

    fix: config

--- config.ts
+++ config.ts
@@ -2,8 +2,5 @@ import { App } from "@slack/bolt";
 
 export const app = new App({
 	signingSecret: process.env.SLACK_SIGNING_SECRET,
-	clientId: process.env.SLACK_CLIENT_ID,
-	clientSecret: process.env.SLACK_CLIENT_SECRET,
-	stateSecret: "my-state-secret",
 	token: process.env.SLACK_BOT_TOKEN,
 });
